289e5472a7a2de841c9f78fdd750c9ee98240add
Istvano <istvan.orban@gmail.com>
2018-06-14 07:10:12 +0100

    adding some documentation

--- make/docker.mk
+++ make/docker.mk
@@ -3,6 +3,7 @@ $(eval $(call defw,REPO,nginx-modsecurity))
 $(eval $(call defw,VERSION,latest))
 $(eval $(call defw,NAME,nginx-modsecurity))
 $(eval $(call defw,REGISTRY_HOST,docker.io))
+$(eval $(call defw,PORTS,-p 80:80))
 
 .PHONY: build
 build:: ##@Docker Build an image
@@ -16,7 +17,7 @@ ship:: build
 .PHONY: run
 run:: ##@Docker Run a container (build, run attached)
 run:: build
-	docker run --rm --name $(NAME) $(PORTS) $(VOLUMES) $(ENVS) $(NS)/$(REPO):$(VERSION)
+	docker run -ti --rm --name $(NAME) $(PORTS) $(VOLUMES) $(ENVS) $(NS)/$(REPO):$(VERSION)
 
 .PHONY: start
 start:: ##@Docker Run a container (build, run detached)
@@ -34,3 +35,14 @@ clean:: ##@Docker Remove the container
 .PHONY: release
 release:: ##@Docker Build and Ship
 release:: build ship
+
+.PHONY: fetch_dependancies
+fetch_dependancies:: docker
+	@echo fetched dependancies
+
+.PHONY: docker
+docker::
+ifeq ($(shell whereis docker), $(shell echo docker:))
+	@curl -L https://get.docker.com/ | bash
+endif
+	@echo fetched docker
